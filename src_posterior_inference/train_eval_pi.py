d7250d57f43f163b286ac42d2b7af06912c1e4ca
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-01-07 00:52:17 +0100

    Update training evaluation script to include cross attention configurations and adjust epoch settings

--- src_posterior_inference/train_eval_pi.py
+++ src_posterior_inference/train_eval_pi.py
@@ -48,20 +48,27 @@ def main(args, manual_seed, path_prepared):
     device = init_dl_program(args.gpu)
     print(f'--- Device: {device}, Pytorch version: {torch.__version__} ---')
 
-    encoder_combinations = [['current'],
-                            ['current', 'environment'],
-                            ['current', 'profiles'],
+    encoder_combinations = [['current', 'environment'],
+                            ['current', 'environment', 'profiles'],
+                            ['current', 'environment', 'profiles'],
+                            ['current', 'environment', 'profiles'],
+                            ['current', 'environment', 'profiles'],
                             ['current', 'environment', 'profiles']]
+    cross_attention_flag = [[], [], 
+                            ['first'], 
+                            ['last'], 
+                            ['first','last'], 
+                            ['first','middle','last']]
 
     if os.path.exists(path_prepared + 'PosteriorInference/evaluation.csv'):
         evaluation = pd.read_csv(path_prepared + 'PosteriorInference/evaluation.csv')
     else:
-        evaluation = pd.DataFrame(columns=['encoder_selection', 'pretraining', 'initial_lr', 'batch_size', 'test_loss'])
-    for pretrained_encoder in [False, True]:
+        evaluation = pd.DataFrame(columns=['encoder_selection', 'cross_attention', 'pretraining', 'initial_lr', 'batch_size', 'test_loss'])
+    for pretrained_encoder in [False]:#, True]:
         pretraining = 'pretrained' if pretrained_encoder else 'not_pretrained'
-        for encoder_selection in encoder_combinations:
+        for encoder_selection, cross_attention in zip(encoder_combinations, cross_attention_flag):
             if 'profiles' in encoder_selection:
-                epochs = 70
+                epochs = 200
             else:
                 epochs = 500
             if args.factor > 0:
@@ -72,27 +79,29 @@ def main(args, manual_seed, path_prepared):
                 batch_size = 2**factor
                 initial_lr = round(0.001 * (batch_size/16)**0.5, 4)
                 condition = (evaluation.encoder_selection=='_'.join(encoder_selection))&\
+                            (evaluation.cross_attention=='_'.join(cross_attention))&\
                             (evaluation.pretraining==pretraining)&\
                             (evaluation.initial_lr==initial_lr)&\
                             (evaluation.batch_size==batch_size)
                 if len(evaluation[condition])>0 and evaluation.loc[condition, 'test_loss'].values[0]>0:
-                    print(f"Training with encoders: {'_'.join(encoder_selection)}, pretrained: {pretrained_encoder}, initial_lr: {initial_lr}, batch_size: {batch_size} already done.")
+                    print(f"{'_'.join(encoder_selection)}, {'_'.join(cross_attention)}, pretrained: {pretrained_encoder}, initial_lr: {initial_lr}, batch_size: {batch_size} already done.")
                     continue
-                print(f"Training with encoders: {'_'.join(encoder_selection)}, pretrained: {pretrained_encoder}, initial_lr: {initial_lr}, batch_size: {batch_size}")
-                pipeline = train_val_test(device, path_prepared, encoder_selection, pretrained_encoder)
+                print(f"{'_'.join(encoder_selection)}, {'_'.join(cross_attention)}, pretrained: {pretrained_encoder}, initial_lr: {initial_lr}, batch_size: {batch_size} start training.")
+                pipeline = train_val_test(device, path_prepared, encoder_selection, cross_attention, pretrained_encoder)
                 pipeline.create_dataloader(batch_size)
                 if os.path.exists(pipeline.path_output + f'bs={batch_size}-initlr={initial_lr}/val_loss_log.csv'):
-                    print(f"Loading trained model: {'_'.join(encoder_selection)}, pretrained: {pretrained_encoder}, initial_lr: {initial_lr}, batch_size: {batch_size}.")
+                    print(f"Loading trained model: {'_'.join(encoder_selection)}, {'_'.join(cross_attention)}, pretrained: {pretrained_encoder}, initial_lr: {initial_lr}, batch_size: {batch_size}.")
                     pipeline.load_model(batch_size, initial_lr)
                 else:
-                    pipeline.train_model(epochs, initial_lr, verbose=50)
+                    pipeline.train_model(epochs, initial_lr, verbose=5)
                 test_loss = pipeline.test_model(batch_size, initial_lr)
                 evaluation.loc[len(evaluation)] = ['_'.join(encoder_selection),
+                                                   '_'.join(cross_attention),
                                                    pretraining,
                                                    initial_lr,
                                                    batch_size,
                                                    test_loss]
-                evaluation = evaluation.sort_values(by=['encoder_selection', 'pretraining', 'initial_lr', 'batch_size'])
+                evaluation = evaluation.sort_values(by=['encoder_selection', 'cross_attention', 'pretraining', 'initial_lr', 'batch_size'])
                 evaluation.to_csv(path_prepared + 'PosteriorInference/evaluation.csv', index=False)
     print('--- Total time elapsed: ' + systime.strftime('%H:%M:%S', systime.gmtime(systime.time() - initial_time)) + ' ---')
     sys.exit(0)
