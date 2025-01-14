134b41008236d08e66467ea062334dca76d80624
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-01-14 11:39:23 +0100

    Reverse sort event categories for evaluation and limit target processing iterations to a maximum of 200

--- src_safety_evaluation/evaluate_safety.py
+++ src_safety_evaluation/evaluate_safety.py
@@ -66,7 +66,7 @@ def main(args, events, manual_seed, path_prepared, path_result):
 
     # Evaluate for each event category
     path_save = path_result + 'EventEvaluation/'
-    event_categories = sorted(os.listdir(path_save))
+    event_categories = sorted(os.listdir(path_save))[::-1]
     for event_cat in event_categories:
         print(f'--- Evaluating {event_cat} ---')
         event_meta = pd.read_csv(path_save + f'{event_cat}/event_meta.csv').set_index('event_id')
@@ -84,7 +84,7 @@ def main(args, events, manual_seed, path_prepared, path_result):
         spacing_list = []
         event_id_list = []
         target_ids = data[data['event_id'].isin(event_meta[event_meta['duration_enough']].index.values)].index.unique(level='target_id').values
-        for target_id in tqdm(target_ids, desc='Target', position=0, dynamic_ncols=False, ascii=True, miniters=len(target_ids)//10):
+        for target_id in tqdm(target_ids, desc='Target', position=0, dynamic_ncols=False, ascii=True, miniters=min(len(target_ids)//10, 200)):
             df = data.loc(axis=0)[target_id, :]
             if len(df)<25: # skip if the target was detected for less than 2.5 seconds
                 continue
