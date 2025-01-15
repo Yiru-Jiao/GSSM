8383a8fdd4c84401f4ec2f0426c107f5d01e5e15
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-01-15 22:25:03 +0100

    Adjust training epochs for encoder selection to optimize performance

--- src_posterior_inference/pi_search_bslr.py
+++ src_posterior_inference/pi_search_bslr.py
@@ -66,10 +66,10 @@ def main(args, manual_seed, path_prepared):
             cross_attention_name = '_'.join(cross_attention) if len(cross_attention)>0 else 'not_crossed'
             initial_lr = 0.0002
             if 'profiles' in encoder_selection:
-                epochs = 15
+                epochs = 10
                 factor_range = range(4, 10) # 16, 32, 64, 128, 256, 512
             else:
-                epochs = 30
+                epochs = 20
                 factor_range = range(5, 11) # 32, 64, 128, 256, 512, 1024
             for factor in factor_range:
                 batch_size = 2**factor
