077e5c8ab195b946ad8db6ba32419dfe26e88daf
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-01-18 12:28:45 +0100

    Add reversed_list argument to parse_args and update cross attention logic for flexibility

--- src_posterior_inference/pi_train_eval.py
+++ src_posterior_inference/pi_train_eval.py
@@ -19,8 +19,10 @@ def parse_args():
     parser.add_argument('--gpu', type=str, default='0', help='The gpu number to use for training and inference (defaults to 0 for CPU only, can be "1,2" for multi-gpu)')
     parser.add_argument('--seed', type=int, default=None, help='The random seed')
     parser.add_argument('--reproduction', type=int, default=1, help='Whether this run is for reproduction, if set to True, the random seed would be fixed (defaults to True)')
+    parser.add_argument('--reversed_list', type=int, default=0, help='Whether this run is for reproduction, if set to True, the random seed would be fixed (defaults to True)')
     args = parser.parse_args()
     args.reproduction = bool(args.reproduction)
+    args.reversed_list = bool(args.reversed_list)
     return args
 
 
@@ -47,30 +49,34 @@ def main(args, manual_seed, path_prepared):
                             ['current', 'environment'],
                             ['current', 'environment', 'profiles'],
                             ['current', 'environment', 'profiles'],
-                            ['current', 'environment', 'profiles'],
                             ['current', 'environment', 'profiles']]
-    cross_attention_flag = [[], [], [], 
-                            ['first'], 
+    cross_attention_flag = [[], [], [],
                             ['first','last'], 
                             ['first','middle','last']]
+    if args.reversed_list:
+        encoder_combinations = encoder_combinations[::-1]
+        cross_attention_flag = cross_attention_flag[::-1]
     
     if os.path.exists(path_prepared + 'PosteriorInference/evaluation.csv'):
         evaluation = pd.read_csv(path_prepared + 'PosteriorInference/evaluation.csv')
     else:
         evaluation = pd.DataFrame(columns=['encoder_selection', 'cross_attention', 'pretraining', 'initial_lr', 'batch_size', 'test_loss'])
-    bslr_search = pd.read_csv(path_prepared + 'PosteriorInference/bslr_search.csv')
-    for pretrained_encoder in [False, True]:
+    # bslr_search = pd.read_csv(path_prepared + 'PosteriorInference/bslr_search.csv')
+    # for pretrained_encoder in [False, True]:
+    for pretrained_encoder in [False]:
         pretraining = 'pretrained' if pretrained_encoder else 'not_pretrained'
         for encoder_selection, cross_attention in zip(encoder_combinations, cross_attention_flag):
             encoder_name = '_'.join(encoder_selection)
             cross_attention_name = '_'.join(cross_attention) if len(cross_attention)>0 else 'not_crossed'
             epochs = 300
 
-            bslr = bslr_search[(bslr_search.encoder_selection==encoder_name)&
-                               (bslr_search.cross_attention==cross_attention_name)&
-                               (bslr_search.pretraining==pretraining)].sort_values(by='avg_val_loss')
-            batch_size = int(bslr['batch_size'].values[0])
-            initial_lr = round(float(bslr['initial_lr'].values[0]), 4)
+            # bslr = bslr_search[(bslr_search.encoder_selection==encoder_name)&
+            #                    (bslr_search.cross_attention==cross_attention_name)&
+            #                    (bslr_search.pretraining==pretraining)].sort_values(by='avg_val_loss')
+            # batch_size = int(bslr['batch_size'].values[0])
+            # initial_lr = round(float(bslr['initial_lr'].values[0]), 4)
+            batch_size = 128
+            initial_lr = 0.0001
 
             condition = (evaluation.encoder_selection==encoder_name)&\
                         (evaluation.cross_attention==cross_attention_name)&\
