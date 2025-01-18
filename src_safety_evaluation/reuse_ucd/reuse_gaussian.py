9b94bc3c20bcce70304f7347f3f1ef1153a1d61e
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-01-18 20:42:25 +0100

    Add reuse_gaussian.py for safety evaluation and unified conflict detection analysis

---
+++ src_safety_evaluation/reuse_ucd/reuse_gaussian.py
@@ -0,0 +1,117 @@
+'''
+This script reuses the Unified Conflict Detection (UCD) model to evaluate the event safety.
+'''
+
+import os
+import sys
+import random
+import time as systime
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import torch
+import argparse
+sys.path.append('./')
+from scipy.special import erf
+from unified_conflit_detection import *
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from src_encoder_pretraining.ssrl_utils.utils_general import fix_seed, init_dl_program
+from joblib import Parallel, delayed
+from src_safety_evaluation.validation_utils.utils_evaluation import *
+
+
+
+# Set input and output paths
+path_input = './Data/InputData/'
+path_output = './Data/OutputData/'
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', type=str, default='0', help='The gpu number to use for training and inference (defaults to 0 for CPU only, can be "1,2" for multi-gpu)')
+    parser.add_argument('--seed', type=int, default=None, help='The random seed')
+    parser.add_argument('--reproduction', type=int, default=1, help='Whether this run is for reproduction, if set to True, the random seed would be fixed (defaults to True)')
+    args = parser.parse_args()
+    args.reproduction = bool(args.reproduction)
+    return args
+
+
+def main(args, manual_seed, path_result):
+    initial_time = systime.time()
+    print('Available cpus:', torch.get_num_threads(), 'available gpus:', torch.cuda.device_count())
+    
+    # Set the random seed
+    if args.reproduction:
+        args.seed = manual_seed # Fix the random seed for reproduction
+    if args.seed is None:
+        args.seed = random.randint(0, 1000)
+    print(f"Random seed is set to {args.seed}")
+    fix_seed(args.seed, deterministic=args.reproduction)
+    
+    # Initialize the deep learning program
+    print(f'--- Cuda available: {torch.cuda.is_available()} ---')
+    if torch.cuda.is_available(): 
+        print(f'--- Cuda device count: {torch.cuda.device_count()}, Cuda device name: {torch.cuda.get_device_name()}, Cuda version: {torch.version.cuda}, Cudnn version: {torch.backends.cudnn.version()} ---')
+    device = init_dl_program(args.gpu)
+    print(f'--- Device: {device}, Pytorch version: {torch.__version__} ---')
+
+    # Read event data
+    path_save = path_result + 'EventEvaluation/'
+    event_categories = sorted(os.listdir(path_save))[::-1]
+    event_categories = [event_cat for event_cat in event_categories if os.path.isdir(path_save + event_cat)]
+    event_data = pd.concat([pd.read_hdf(path_save + f'{event_cat}/event_data.h5', key='data') for event_cat in event_categories]).reset_index()
+    if os.path.exists(path_result + 'Analyses/EventMeta.csv'):
+        event_meta = pd.read_csv(path_result + 'Analyses/EventMeta.csv', index_col=0)
+    else:
+        event_meta = pd.concat([pd.read_csv(path_save + f'{event_cat}/event_meta.csv') for event_cat in event_categories], ignore_index=True).set_index('event_id')
+    danger_start = np.maximum(event_meta['impact_timestamp'].values-3000, event_meta['start_timestamp'].values)
+    danger_end = np.minimum(event_meta['impact_timestamp'].values+500, event_meta['end_timestamp'].values)
+    event_meta['danger_start'] = danger_start
+    event_meta['danger_end'] = danger_end
+    assert np.all(np.isin(event_data['event_id'].unique(), event_meta.index.values))
+
+    event_data['vx_ego'] = event_data['v_ego']*event_data['hx_ego']
+    event_data['vy_ego'] = event_data['v_ego']*event_data['hy_ego']
+    event_data['vx_sur'] = event_data['v_sur']*event_data['hx_sur']
+    event_data['vy_sur'] = event_data['v_sur']*event_data['hy_sur']
+    veh_dimensions = event_meta[['ego_width','ego_length','target_width','target_length']].copy()
+    condition = event_meta[['target_width','target_length']].isna().any(axis=1)
+    veh_dimensions.loc[condition, ['target_width','target_length']] = event_meta.loc[condition, ['other_width','other_length']].values
+    avg_width = np.nanmean(veh_dimensions['ego_width'].values)
+    avg_length = np.nanmean(veh_dimensions['ego_length'].values)
+    for var in ['ego_width','ego_length','target_width','target_length']:
+        veh_dimensions.loc[veh_dimensions[var].isna(), var] = avg_width if 'width' in var else avg_length
+    event_data[['length_ego','length_sur']] = veh_dimensions.loc[event_data['event_id'].values, ['ego_length','target_length']].values
+
+    if os.path.exists(path_save + 'unified_conflict_detection.h5'):
+        safety_evaluation = pd.read_hdf(path_save + 'unified_conflict_detection.h5', key='data')
+    else:
+        safety_evaluation = UCD(event_data, device)
+        safety_evaluation.to_hdf(path_save + f'unified_conflict_detection.h5', key='data', mode='w')
+
+    ucd_thresholds = np.round(np.arange(2,101)*1.3)
+    print('--- Analyzing ---')
+    progress_bar = tqdm(ucd_thresholds, desc='UCD', ascii=True, dynamic_ncols=False, miniters=10)
+    ucd_records = Parallel(n_jobs=-1)(delayed(parallel_records)(threshold, safety_evaluation, event_data, event_meta[event_meta['duration_enough']], 'SSSE') for threshold in progress_bar)
+    ucd_records = pd.concat(ucd_records).reset_index()
+    ucd_records['indicator'] = 'UCD'
+    ucd_records['model'] = 'ucd'
+
+    ucd_records.loc[ucd_records['danger_recorded'].isna(), 'danger_recorded'] = False
+    ucd_records.loc[ucd_records['danger_evaluated'].isna(), 'danger_evaluated'] = False
+    ucd_records.loc[ucd_records['safety_recorded'].isna(), 'safety_recorded'] = False
+    ucd_records[['danger_recorded', 'danger_evaluated', 'safety_recorded']] = ucd_records[['danger_recorded', 'danger_evaluated', 'safety_recorded']].astype(bool)
+    ucd_records.to_hdf(path_result + 'Analyses/UCDWarning.h5', key='results', mode='w')
+
+    print('--- Total time elapsed: ' + systime.strftime('%H:%M:%S', systime.gmtime(systime.time() - initial_time)) + ' ---')
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    sys.stdout.reconfigure(line_buffering=True)
+    args = parse_args()
+
+    manual_seed = 131
+    path_result = './ResultData/'
+    
+    main(args, manual_seed, path_result)
---
+++ src_safety_evaluation/reuse_ucd/unified_conflit_detection.py
@@ -0,0 +1,138 @@
+'''
+This script is reused from UnifiedConflictDetection https://github.com/Yiru-Jiao/UnifiedConflictDetection
+'''
+
+import os
+import sys
+import torch
+import gpytorch
+import numpy as np
+from tqdm import tqdm
+from scipy.special import erf
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from src_data_preparation.represent_utils.coortrans import coortrans
+coortrans = coortrans()
+
+
+# SVGP model: Sparse Variational Gaussian Process
+class SVGP(gpytorch.models.ApproximateGP):
+    def __init__(self, inducing_points):
+
+        # Determine variational distribution and strategy
+        # Number of inducing points is better to be smaller than 1000 to speed up the training
+        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(inducing_points.size(0))
+        variational_strategy = gpytorch.variational.VariationalStrategy(
+            self, inducing_points, variational_distribution, learn_inducing_locations=True
+            )
+        super(SVGP, self).__init__(variational_strategy)
+
+        # Mean module
+        self.mean_module = gpytorch.means.ConstantMean()
+
+        # Kernel module
+        mixture_kernel = gpytorch.kernels.SpectralMixtureKernel(num_mixtures=10, ard_num_dims=10)
+        rbf_kernel = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RQKernel(ard_num_dims=10))
+        self.covar_module = mixture_kernel + rbf_kernel
+
+        # To make mean positive
+        self.softplus = torch.nn.Softplus()
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        mean_x = self.softplus(mean_x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+
+def define_model(num_inducing_points, device):
+    # Create representative points for the input space
+    # This is defined when training. Don't change it when applying the model.
+    inducing_points = np.concatenate([np.random.uniform(4.,12.,(num_inducing_points,1)), # length_ego
+                                      np.random.uniform(4.,12.,(num_inducing_points,1)), # length_sur
+                                      np.random.uniform(-1,1,(num_inducing_points,1)), # hx_sur
+                                      np.random.uniform(-1,1,(num_inducing_points,1)), # hy_sur
+                                      np.random.uniform(0.,20.,(num_inducing_points,1)), # delta_v
+                                      np.random.uniform(0.,400.,(num_inducing_points,1)), # delta_v2
+                                      np.random.uniform(0.,3000.,(num_inducing_points,1)), # v_ego2
+                                      np.random.uniform(0.,3000.,(num_inducing_points,1)), # v_sur2
+                                      np.random.uniform(-5.5,5.5,(num_inducing_points,1)), # acc_ego
+                                      np.random.uniform(-np.pi,np.pi,(num_inducing_points,1))], # rho
+                                      axis=1)
+    inducing_points = torch.from_numpy(inducing_points).float()
+
+    # Define the model
+    model = SVGP(inducing_points)
+    # Define the likelihood of the model
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+
+    ## Load trained model
+    model_path = 'src_safety_evaluation/reuse_ucd'
+    model.load_state_dict(torch.load(f'{model_path}/model_52epoch.pth', map_location=torch.device(device), weights_only=True))
+    likelihood.load_state_dict(torch.load(f'{model_path}/likelihood_52epoch.pth', map_location=torch.device(device), weights_only=True))
+    model.eval()
+    likelihood.eval()
+    model = model.to(device)
+    likelihood = likelihood.to(device)
+    print('Model loaded successfully.')
+
+    return model, likelihood
+
+
+def lognormal_cdf(x, mu, sigma):
+    return 1/2+1/2*erf((np.log(x)-mu)/sigma/np.sqrt(2))
+
+
+def extreme_cdf(x, mu, sigma, n=10):
+    return (1-lognormal_cdf(x,mu,sigma))**n
+
+
+def UCD(data, device):
+    # Mirror the coordinates as the model is trained on highD where the y-axis points downwards
+    data = data.rename(columns={'x_ego':'y_ego', 'y_ego':'x_ego', 'x_sur':'y_sur', 'y_sur':'x_sur',
+                                'vx_ego':'vy_ego', 'vy_ego':'vx_ego', 'vx_sur':'vy_sur', 'vy_sur':'vx_sur',
+                                'hx_ego':'hy_ego', 'hy_ego':'hx_ego', 'hx_sur':'hy_sur', 'hy_sur':'hx_sur'})
+    data['psi_ego'] = coortrans.angle(1, 0, data['hx_ego'], data['hy_ego'])
+    data['psi_sur'] = coortrans.angle(1, 0, data['hx_sur'], data['hy_sur'])
+
+    ## Transform coordinates and formulate input data
+    data['delta_v'] = np.sqrt((data['vx_ego']-data['vx_sur'])**2 + (data['vy_ego']-data['vy_sur'])**2)
+    data['delta_v2'] = data['delta_v']**2
+    data['v_ego2'] = data['v_ego']**2
+    data['v_sur2'] = data['v_sur']**2
+    data_view_ego = coortrans.transform_coor(data, view='i')
+    heading_sur = data_view_ego[['target_id','time','hx_sur','hy_sur']]
+    data_relative = coortrans.transform_coor(data, view='relative')
+    rho = coortrans.angle(1, 0, data_relative['x_sur'], data_relative['y_sur']).reset_index().rename(columns={0:'rho'})
+    rho[['target_id','time']] = data_relative[['target_id','time']]
+    interaction_context = data.drop(columns=['hx_sur','hy_sur']).merge(heading_sur, on=['target_id','time']).merge(rho, on=['target_id','time'])
+    features = ['length_ego','length_sur','hx_sur','hy_sur','delta_v','delta_v2','v_ego2','v_sur2','acc_ego','rho']
+    interaction_context = interaction_context[features+['event_id','target_id','time']].sort_values(['target_id','time'])
+    data_relative = data_relative.merge(interaction_context[['target_id','time']], on=['target_id','time']).sort_values(['target_id','time'])
+    proximity = np.sqrt(data_relative['x_sur']**2 + data_relative['y_sur']**2).values
+
+    ## Load trained model
+    model, likelihood = define_model(100, device)
+
+    ## Compute mu_list, sigma_list
+    chunk_size = 2000
+    mu_list, sigma_list = [], []
+    with torch.no_grad(), gpytorch.settings.fast_pred_var():
+        for chunk in tqdm(range(0, len(interaction_context), chunk_size), desc='Inferring', ascii=True, dynamic_ncols=False, miniters=100):
+            chunk_data = interaction_context[features].values[chunk:chunk+chunk_size]
+            f_dist = model(torch.Tensor(chunk_data).to(device))
+            y_dist = likelihood(f_dist)
+            mu, sigma = y_dist.mean.cpu().numpy(), y_dist.variance.sqrt().cpu().numpy()
+            mu_list.append(mu)
+            sigma_list.append(sigma)
+    mu_list = np.concatenate(mu_list)
+    sigma_list = np.concatenate(sigma_list)
+    max_intensity = np.log(0.5)/np.log(1-lognormal_cdf(proximity, mu_list, sigma_list)+1e-6)
+    max_intensity = np.maximum(1., max_intensity)
+
+    results = interaction_context[['event_id','target_id','time']].copy()
+    results['proximity'] = proximity
+    results['mu'] = mu_list
+    results['sigma'] = sigma_list
+    results['intensity'] = max_intensity
+    return results
+
