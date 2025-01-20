5804ab54c58e98e9ef40ad2a84ff0a2d3b6c0d7a
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-01-20 15:55:03 +0100

    Update path_processed to include dataset-specific directory for SHRP2

--- src_safety_evaluation/validation_utils/utils_features.py
+++ src_safety_evaluation/validation_utils/utils_features.py
@@ -13,7 +13,7 @@ coortrans = coortrans()
 
 manual_seed = 131
 path_prepared = './PreparedData/'
-path_processed = './ProcessedData/'
+path_processed = './ProcessedData/SHRP2/'
 
 
 def read_data(event_cat, single_file=True, path_processed=path_processed):
