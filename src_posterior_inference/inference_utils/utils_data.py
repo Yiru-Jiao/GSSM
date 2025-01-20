b2bbce87e392502e49274971273faf2c75aad50f
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-01-20 15:54:32 +0100

    Refactor DataOrganiser to support multiple datasets and improve data reading logic

--- src_posterior_inference/inference_utils/utils_data.py
+++ src_posterior_inference/inference_utils/utils_data.py
@@ -36,9 +36,10 @@ def get_scaler(dataset_dir, feature):
 
 
 class DataOrganiser(Dataset):
-    def __init__(self, split, encoder_selection, path_prepared):
+    def __init__(self, split, dataset, encoder_selection, path_prepared):
         super(DataOrganiser, self).__init__()
         self.split = split
+        self.dataset = dataset
         if encoder_selection=='all':
             encoder_selection = ['current', 'environment', 'profiles']
         self.encoder_selection = encoder_selection
@@ -69,25 +70,30 @@ class DataOrganiser(Dataset):
         return combine_features
 
     def read_data(self,):
-        print(f'Reading data for {self.encoder_selection} {self.split}...')
+        print(f'Reading data for {self.dataset} {self.encoder_selection} {self.split}...')
         self.data = []
-        if self.split!='train':
-            X_current = pd.read_hdf(os.path.join(self.path_prepared, 'Segments/current_features_'+self.split+'.h5'), key='features')
-        else:
-            X_current_conflict = pd.read_hdf(os.path.join(self.path_prepared, 'Segments/current_features_train.h5'), key='features')
-            X_current_safe = pd.read_hdf(os.path.join(self.path_prepared, 'Segments/current_features_safe.h5'), key='features')
-            X_current = pd.concat([X_current_conflict, X_current_safe], ignore_index=True)
+        X_current = []
+        scene_id = 0
+        for dataset in self.dataset:
+            x_current = pd.read_hdf(f'{self.path_prepared}Segments/{dataset}/current_features_{dataset}_{self.split}.h5', key='features')
+            x_current['scene_id'] = x_current['scene_id'] + scene_id
+            scene_id = x_current['scene_id'].max() + 1
+            X_current.append(x_current)
+        X_current = pd.concat(X_current, ignore_index=True)
+        X_current = X_current.sort_values('scene_id').reset_index(drop=True)
         self.scene_ids = X_current['scene_id'].values
         variables = ['l_ego','l_sur','delta_v','psi_sur','acc_ego','v_ego2','v_sur2','delta_v2','rho']
         self.data.append(torch.from_numpy(self.current_scaler.transform(X_current[variables].values)).float())
 
         if 'environment' in self.encoder_selection:
-            if self.split!='train':
-                X_environment = pd.read_hdf(os.path.join(self.path_prepared, 'Segments/environment_features_'+self.split+'.h5'), key='features')
-            else:
-                X_environment_conflict = pd.read_hdf(os.path.join(self.path_prepared, 'Segments/environment_features_train.h5'), key='features')
-                X_environment_safe = pd.read_hdf(os.path.join(self.path_prepared, 'Segments/environment_features_safe.h5'), key='features')
-                X_environment = pd.concat([X_environment_conflict, X_environment_safe], ignore_index=True)
+            X_environment = []
+            scene_id = 0
+            for dataset in self.dataset:
+                x_environment = pd.read_hdf(f'{self.path_prepared}Segments/{dataset}/environment_features_{dataset}_{self.split}.h5', key='features')
+                x_environment['scene_id'] = x_environment['scene_id'] + scene_id
+                scene_id = x_environment['scene_id'].max() + 1
+                X_environment.append(x_environment)
+            X_environment = pd.concat(X_environment, ignore_index=True)
             X_environment = X_environment.sort_values('scene_id').reset_index(drop=True)
             assert np.all(X_current['scene_id'].values==X_environment['scene_id'].values)
             X_environment = X_environment.drop(columns=['scene_id', 'event_id', 'target_id'])
@@ -95,15 +101,17 @@ class DataOrganiser(Dataset):
             self.data.append(torch.from_numpy(X_environment.values).float())
         
         if 'profiles' in self.encoder_selection:
-            if self.split!='train':
-                X_profiles = pd.read_hdf(os.path.join(self.path_prepared, 'Segments/profiles_'+self.split+'.h5'), key='profiles')
-            else:
-                X_profiles_conflict = pd.read_hdf(os.path.join(self.path_prepared, 'Segments/profiles_train.h5'), key='profiles')
-                X_profiles_safe = pd.read_hdf(os.path.join(self.path_prepared, 'Segments/profiles_safe.h5'), key='profiles')
-                X_profiles = pd.concat([X_profiles_conflict, X_profiles_safe])
+            X_profiles = []
+            scene_id = 0
+            for dataset in self.dataset:
+                x_profiles = pd.read_hdf(f'{self.path_prepared}Segments/{dataset}/profiles_{dataset}_{self.split}.h5', key='profiles')
+                x_profiles['scene_id'] = x_profiles['scene_id'] + scene_id
+                scene_id = x_profiles['scene_id'].max() + 1
+                X_profiles.append(x_profiles)
+            X_profiles = pd.concat(X_profiles)
             X_profiles = X_profiles.sort_values(['scene_id', 'time']).reset_index(drop=True)
             assert np.all(X_current['scene_id'].values==X_profiles['scene_id'].drop_duplicates().values)
-            X_profiles = X_profiles[['v_ego','omega_ego','v_sur']].values.reshape(-1, 20, 3)
+            X_profiles = X_profiles[['v_ego','v_sur','angle']].values.reshape(-1, 20, 3)
             X_profiles = self.profiles_scaler.transform(X_profiles.reshape(-1, 3)).reshape(X_profiles.shape)
             self.data.append(torch.from_numpy(X_profiles).float())
 
