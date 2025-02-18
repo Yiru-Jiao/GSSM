2659bb2bd292b4f43bfd44d71edbcb400495e6b5
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-02-18 22:21:35 +0100

    Refactor forward method documentation in LSTMEncoder for clarity and consistency

--- src_encoder_pretraining/modules/encoder.py
+++ src_encoder_pretraining/modules/encoder.py
@@ -22,7 +22,7 @@ class LSTMEncoder(nn.Module):
         self.single_output = single_output
         self.lstm = nn.LSTM(input_dims, hidden_dims, num_layers, batch_first=True)
 
-    def forward(self, x): # x: (batch_size, seq_length=20, feature_dims=3)
+    def forward(self, x): # x: (batch_size, seq_length, feature_dims=3)
         # Initialize hidden and cell states
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dims).to(x.device)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dims).to(x.device)
@@ -31,7 +31,7 @@ class LSTMEncoder(nn.Module):
         output, (hidden, cell) = self.lstm(x, (h0, c0)) # hidden: (num_layers, batch_size, hidden_dims)
         if self.single_output:
             # Only return the hidden state of the last LSTM layer
-            return hidden[-1].view(x.size(0), x.size(1), -1) # (batch_size, seq_length, hidden_dims//seq_length)
+            return hidden[-1].view(x.size(0), 20, -1) # (batch_size, 20, hidden_dims//20)
         else:
             # Return all
             return output, (hidden, cell)
