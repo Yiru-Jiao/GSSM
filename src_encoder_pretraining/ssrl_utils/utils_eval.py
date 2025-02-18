5230f90b4ea5ec8916bd1f5f20a8fe871f34a667
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-02-18 13:05:25 +0100

    Refactor evaluate function to streamline latent space encoding logic based on states presence

--- src_encoder_pretraining/ssrl_utils/utils_eval.py
+++ src_encoder_pretraining/ssrl_utils/utils_eval.py
@@ -92,14 +92,10 @@ class Multi_Evaluation:
 
 
 def evaluate(data, model, batch_size, local=False, states=None):
-    # encode data into latent space
-    if local:
-        latent = model.encode(data, batch_size=batch_size).detach().cpu().numpy() # (N, T, P)
-    else:
-        if states is None:
-            latent = model.encode(data, batch_size=batch_size, encoding_window='full_series').detach().cpu().numpy() # (N, P)
-        else:
-            latent = model.encode(states, batch_size=batch_size, encoding_window='full_series').detach().cpu().numpy()
+    if states is None:
+        latent = model.encode(data, batch_size=batch_size).detach().cpu().numpy()
+    else: # states include current, (environment), and profiles
+        latent = model.encode(states, batch_size=batch_size).detach().cpu().numpy()
 
     evaluator = Multi_Evaluation(data, latent)
     ev_result = evaluator.get_multi_evals(local)
