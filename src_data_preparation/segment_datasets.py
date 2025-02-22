bbe68af3f3f3b0ddcd4f4e09dad8a49db86effd4
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-02-22 16:28:17 +0100

    Refactor dataset processing in segment_datasets.py by reordering dataset list and removing unused plotting code

--- src_data_preparation/segment_datasets.py
+++ src_data_preparation/segment_datasets.py
@@ -7,11 +7,6 @@ import sys
 import multiprocessing
 import time as systime
 import numpy as np
-import matplotlib.pyplot as plt
-font = {'family' : 'Arial',
-        'size'   : 9}
-plt.rc('font', **font)
-from matplotlib.ticker import ScalarFormatter
 from represent_utils.utils_data_segmentation import ContextSegmenter, read_dataset
 
 
@@ -19,7 +14,7 @@ def main(path_prepared, path_processed):
     initial_time = systime.time()
     print(f'Available cores for parallel processing: {multiprocessing.cpu_count()}')
 
-    for dataset in ['highD', 'INTERACTION', 'Argoverse', 'SafeBaseline']:
+    for dataset in ['highD', 'INTERACTION', 'SafeBaseline', 'Argoverse']:
         path_save = f'{path_prepared}{dataset}/'
         os.makedirs(path_save, exist_ok=True)
         if os.path.exists(path_save + 'speed_distribution_' + dataset + '.pdf'):
@@ -32,8 +27,8 @@ def main(path_prepared, path_processed):
         '''
         highD: 355,570 train scenes (min. dist. 0.74 m) + 87,062 val scenes (min. dist. 2.95 m)
         INTERACTION: train 456,849 scenes (min. dist. 2.54 m) + 115,332 val scenes (min. dist. 2.45 m)
-        Argoverse: 1,037,541 train scenes (min. dist. 0.04 m) + 258,493 val scenes (min. dist. 0.22 m)
         SafeBaseline: 469,087 train scenes (min. dist. 1.64 m) + 115,898 val scenes (min. dist. 1.90 m)
+        Argoverse: 1,037,541 train scenes (min. dist. 0.04 m) + 258,493 val scenes (min. dist. 0.22 m)
         '''
         event_ids = data_both['event_id'].unique()
         len_event_ids = len(event_ids)
@@ -44,11 +39,6 @@ def main(path_prepared, path_processed):
 
         # Segment and save scenes
         initial_scene_id = 0
-        fig, axes = plt.subplots(1, 2, figsize=(5., 2.), constrained_layout=True)
-        if dataset=='Argoverse' or dataset=='INTERACTION':
-            bins = np.linspace(0, 20, 21)
-        else:
-            bins = np.linspace(0, 40, 31)
         for data, suffix in zip([data_train, data_val], ['train', 'val']):
             print('Segmenting ' + dataset + ' ' + suffix + ' set...')
             segmenter = ContextSegmenter(data, initial_scene_id, dataset)
@@ -60,19 +50,6 @@ def main(path_prepared, path_processed):
             print(f'Number of scenes: {initial_scene_id - segmenter.initial_scene_id}')
             print(f"Minimum dist.: {segmenter.current_features_set['s'].min():.2f}")
             print(f"Unique scene ids in current features set: {segmenter.current_features_set['scene_id'].nunique()}, should be the same as the profiles set: {segmenter.profiles_set['scene_id'].nunique()}")
-            ## save a plot of speed distribution
-            ax = axes[0] if suffix=='train' else axes[1]
-            ax.hist(segmenter.profiles_set['v_ego'], bins=bins, alpha=0.5, label='Ego vehicle')
-            ax.hist(segmenter.profiles_set['v_sur'], bins=bins, alpha=0.5, label='Surrounding vehicles')
-            ax.set_xlabel('Speed (m/s)')
-            ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-            ax.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
-            ax.set_ylabel('Frequency')
-        handles, labels = ax.get_legend_handles_labels()
-        fig.legend(handles, labels, loc='lower center', ncol=2, bbox_to_anchor=(0.5, -0.1))
-        axes[0].set_title('Train set')
-        axes[1].set_title('Val set')
-        fig.savefig(path_save + f'speed_distribution_{dataset}.pdf', bbox_inches='tight', dpi=600)
 
     print('--- Total time elapsed: ' + systime.strftime('%H:%M:%S', systime.gmtime(systime.time() - initial_time)) + ' ---')
     sys.exit(0)
