1996cb3400dcbc92baf69252b77be5c681bfb016
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-02-23 09:12:37 +0100

    Adjust iteration calculation based on sample size thresholds for improved performance

--- src_encoder_pretraining/clt_model.py
+++ src_encoder_pretraining/clt_model.py
@@ -92,14 +92,10 @@ class spclt():
         if n_iters is None and n_epochs is None:
             num_samples = train_data.shape[0]
             n_iters = num_samples * 32 / self.batch_size
-            sample_bounds = [100, 500, 1000, 10000, 100000, 1000000]
-            coefs = [2, 1, 1/2, 1/4, 1/8, 1/16]
-            for bound, coef in zip(sample_bounds, coefs):
-                if num_samples < bound:
-                    n_iters = int(n_iters * coef)
-                    break
-            if num_samples >= sample_bounds[-1]:
+            if num_samples < 1000000:
                 n_iters = int(n_iters / 64)
+            else:
+                n_iters = int(n_iters / 128)
             print(f'Number of iterations is set to {n_iters}.')
 
         # define a progress bar
