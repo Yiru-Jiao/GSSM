875ab16e2fba84249d9c5d87b021946253d35e3a
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-02-23 10:14:23 +0100

    Update initial learning rate in training configuration for improved model performance

--- src_safety_evaluation/reuse_ucd/train_gaussian.py
+++ src_safety_evaluation/reuse_ucd/train_gaussian.py
@@ -44,7 +44,7 @@ def main(args, manual_seed):
 
     # Read event data
     beta = 5
-    initial_lr = 0.02
+    initial_lr = 0.025
     batch_size = 2048
     num_qepochs = 500
     num_inducing_points = 100
