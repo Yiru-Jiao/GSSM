69a51836782ef5f0910b64b9b08e5a922be4dc92
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-02-26 13:51:34 +0100

    Reduce output dimensions in CurrentEncoder and EnvEncoder from 128 to 64 for improved model efficiency

--- src_encoder_pretraining/ae_model.py
+++ src_encoder_pretraining/ae_model.py
@@ -28,14 +28,14 @@ class model(nn.Module):
     def __init__(self, encoder_name):
         super(model, self).__init__()
         if encoder_name == 'current':
-            self.encoder = CurrentEncoder(input_dims=1, output_dims=128)
-            self.decoder = shared_decoder(input_dims=12*128, output_dims=12)
+            self.encoder = CurrentEncoder(input_dims=1, output_dims=64)
+            self.decoder = shared_decoder(input_dims=12*64, output_dims=12)
         elif encoder_name == 'current+acc':
-            self.encoder = CurrentEncoder(input_dims=1, output_dims=128)
-            self.decoder = shared_decoder(input_dims=13*128, output_dims=13)
+            self.encoder = CurrentEncoder(input_dims=1, output_dims=64)
+            self.decoder = shared_decoder(input_dims=13*64, output_dims=13)
         elif encoder_name == 'environment':
-            self.encoder = EnvEncoder(input_dims=27, output_dims=128)
-            self.decoder = shared_decoder(input_dims=128, output_dims=27)
+            self.encoder = EnvEncoder(input_dims=27, output_dims=64)
+            self.decoder = shared_decoder(input_dims=64, output_dims=27)
         else:
             ValueError("Undefined encoder name: should be among 'current', 'current+acc', 'environment'.")
 
