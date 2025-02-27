000d9ebdc4332ee05f40839176042ef65b299da2
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-02-27 09:48:45 +0100

    Fix LSTMEncoder forward method to correctly handle input sequences and improve tensor contiguity

--- src_encoder_pretraining/modules/encoder.py
+++ src_encoder_pretraining/modules/encoder.py
@@ -14,9 +14,9 @@ class LSTMEncoder(nn.Module):
         self.LSTMs = nn.ModuleList([nn.LSTM(input_dims, hidden_dims, num_layers=num_layers, batch_first=True)]*5)
 
     def forward(self, x): # x: (batch_size, 25, feature_dims=4)
-        # for per 0.5 sec (5 time steps, in total 5 time blocks)
+        # in total 5 time blocks, each for the passed 0.5, 1, 1.5, 2, 2.5 seconds
         for i, lstm in enumerate(self.LSTMs):
-            sub_x = x[:, :(i+1)*5, :] # (batch_size, [5, 10, 15, 20, 25], feature_dims)
+            sub_x = x[:, -(i+1)*5:, :] # (batch_size, [5, 10, 15, 20, 25], feature_dims)
             h0 = torch.zeros(self.num_layers, sub_x.size(0), self.hidden_dims).to(x.device)
             c0 = torch.zeros(self.num_layers, sub_x.size(0), self.hidden_dims).to(x.device)
             _, (sub_hidden, _) = lstm(sub_x, (h0, c0)) # hidden: (num_layers, batch_size, hidden_dims)
@@ -24,6 +24,6 @@ class LSTMEncoder(nn.Module):
                 hidden = sub_hidden[-1] # (batch_size, hidden_dims)
             else:
                 hidden = torch.cat((hidden, sub_hidden[-1]), dim=0) # (5, batch_size, hidden_dims)
-        hidden = hidden.permute(1, 0, 2)
+        hidden = hidden.permute(1, 0, 2).contiguous()
         return hidden # (batch_size, 5, hidden_dims)
 
