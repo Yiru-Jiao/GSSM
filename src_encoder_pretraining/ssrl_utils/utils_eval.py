63cb73327647bcf568d30e5456a0dee1b01b83df
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-03-02 10:34:54 +0100

    Refactor local evaluation: optimize sample processing and improve distance matrix calculations for enhanced accuracy

--- src_encoder_pretraining/ssrl_utils/utils_eval.py
+++ src_encoder_pretraining/ssrl_utils/utils_eval.py
@@ -4,6 +4,7 @@ This script includes utility functions for evaluation of the pretrained encoders
 
 import os
 import sys
+import torch
 from tqdm import tqdm
 import numpy as np
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -36,37 +37,43 @@ class Multi_Evaluation:
                                  'mean_dist_mrre':0., 
                                  'mean_trustworthiness':0, 
                                  'mean_continuity':0.}
-            
-            N = self.data.shape[1] # time series length
-            sample_indices = np.arange(self.data.shape[0])
+            assert self.data.shape[1]==5, 'Local evaluation is only applied to time series encoding and the sequence length should have been set 5.'
+
+            sample_indices = np.arange(0, self.data.shape[0], 10)
             sample_count = 0
             dist_mat_measure = {'local_distmat_rmse': 0}
-            for sample_index in tqdm(sample_indices, desc='Local evaluation', ascii=True, miniters=100):
-                data = self.data[sample_index].reshape(N, -1)
-                latent = self.latent[sample_index].reshape(N, -1)
-                dist_mat_X = get_EUC(data)
-                dist_mat_Z = get_EUC(latent)
-                if dist_mat_X.max()-dist_mat_X.min() == 0 or dist_mat_Z.max()-dist_mat_Z.min() == 0:
-                    continue
-                else:
-                    dist_mat_X = abs((dist_mat_X - dist_mat_X.min()) / (dist_mat_X.max() - dist_mat_X.min()))
-                    dist_mat_Z = abs((dist_mat_Z - dist_mat_Z.min()) / (dist_mat_Z.max() - dist_mat_Z.min()))
+            for i in tqdm(range(5), desc='Local evaluation', ascii=True):
+                # each dimension represents for the passed 0.5, 1, 1.5, 2, 2.5 seconds
+                # latent need to stack into (B, 5x, 64) to map with the original time series
+                sub_l = torch.cat([self.latent[:,[i],:]]*(5*(i+1)), dim=1) # (B, [5, 10, 15, 20, 25], 64)
+                sub_x = self.data[:, -(i+1)*5:, :] # (B, [5, 10, 15, 20, 25], 4)
+                N = sub_x.shape[1]
+                for sample_index in sample_indices:
+                    data = sub_x[sample_index].reshape(N, -1)
+                    latent = sub_l[sample_index].reshape(N, -1)
+                    dist_mat_X = get_EUC(data)
+                    dist_mat_Z = get_EUC(latent)
+                    if dist_mat_X.max()-dist_mat_X.min() == 0 or dist_mat_Z.max()-dist_mat_Z.min() == 0:
+                        continue
+                    else:
+                        dist_mat_X = abs((dist_mat_X - dist_mat_X.min()) / (dist_mat_X.max() - dist_mat_X.min()))
+                        dist_mat_Z = abs((dist_mat_Z - dist_mat_Z.min()) / (dist_mat_Z.max() - dist_mat_Z.min()))
 
-                dist_mat_measure['local_distmat_rmse'] += np.sqrt(np.mean((dist_mat_X - dist_mat_Z)**2))
+                    dist_mat_measure['local_distmat_rmse'] += np.sqrt(np.mean((dist_mat_X - dist_mat_Z)**2))
 
-                ks = self.define_ks(dist_mat_X)
-                calc = MeasureCalculator(dist_mat_X, dist_mat_Z, max(ks))
+                    ks = self.define_ks(dist_mat_X)
+                    calc = MeasureCalculator(dist_mat_X, dist_mat_Z, max(ks))
 
-                dep_measures = calc.compute_measures_for_ks(ks)
-                mean_dep_measures = {'mean_'+key: np.nanmean(values) for key, values in dep_measures.items()}
-                for key, value in mean_dep_measures.items():
-                    dep_measures_list[key] += value
+                    dep_measures = calc.compute_measures_for_ks(ks)
+                    mean_dep_measures = {'mean_'+key: np.nanmean(values) for key, values in dep_measures.items()}
+                    for key, value in mean_dep_measures.items():
+                        dep_measures_list[key] += value
 
-                sample_count += 1
-                if sample_count >= 500:
-                    break
+                    sample_count += 1
+                    if sample_count >= 500:
+                        break
             
-            dist_mat_measure['local_distmat_rmse'] /= sample_count
+            dist_mat_measure['local_distmat_rmse'] /= (sample_count*5)
             dep_measures = {'local_'+key: value/sample_count for key, value in dep_measures_list.items()}
             results = {**dist_mat_measure, **dep_measures}
         else:
