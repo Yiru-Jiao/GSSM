31a2e1414851901f34ac09762d9e2bd629f74780
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-03-07 12:40:42 +0100

    Increase patience parameter in learning rate scheduler for improved training stability

--- src_posterior_inference/inference_utils/utils_train_eval_test.py
+++ src_posterior_inference/inference_utils/utils_train_eval_test.py
@@ -245,7 +245,7 @@ class train_val_test():
                     self.later_loss_func = SmoothLogNormalNLL(beta).to(self.device)
                     self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.initial_lr)
                     self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-                        self.optimizer, mode='min', factor=0.6, patience=5, cooldown=10,
+                        self.optimizer, mode='min', factor=0.6, patience=10, cooldown=10,
                         threshold=1e-3, threshold_mode='rel', verbose='deprecated', min_lr=self.initial_lr*0.6**15
                     )
                     self.lr_reduced = True
