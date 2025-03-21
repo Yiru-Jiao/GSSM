9a14a5aee3ff5072e6364ed1f91901a0b97d0fe2
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-03-21 11:29:03 +0100

    Refactor model handling and event processing in attribute_intensity.py for improved clarity and flexibility

--- src_safety_evaluation/attribute_intensity.py
+++ src_safety_evaluation/attribute_intensity.py
@@ -52,71 +52,77 @@ def main(args, manual_seed, path_prepared, path_result):
     path_save = path_result + 'FeatureAttribution/'
     os.makedirs(path_save, exist_ok=True)
 
-    # Define the model to be used for attribution
-    dataset = ['SafeBaseline']
-    encoder_selection = ['current+acc','environment','profiles']
-    pretrained_encoder = False
+    models = ['SafeBaseline_current_not_pretrained',
+              'SafeBaseline_current_environment_not_pretrained',
+              'SafeBaseline_current_environment_profiles_not_pretrained']
+    for model in models:
+        # Define the model to be used for attribution
+        dataset = ['SafeBaseline']
+        encoder_selection = model.split('SafeBaseline_')[-1].split('_not_pretrained')[0].split('_')
+        pretrained_encoder = False
 
-    pipeline = train_val_test(device, path_prepared, dataset, encoder_selection, pretrained_encoder, single_output='intensity')
-    pipeline.load_model()
-    tokenizer = pipeline.model.combi_encoder
-    decoder = pipeline.model.AttentionDecoder
-    if not pipeline.model.training:
-        print('The model is correctly loaded under evaluation mode.')
-    else:
-        print('The model is not under evaluation mode, please check whether the model is correctly loaded.')
-        sys.exit(0)
-
-    # Define sample generator
-    sampler = get_sample(encoder_selection, path_result)
-    event_id_list = sampler.event_id_list.reset_index()
-
-    # Cluster representative training samples
-    pipeline.create_dataloader(batch_size=1024)
-    kmeans = MiniBatchKMeans(n_clusters=1024, random_state=manual_seed, batch_size=1024)
-    for batch, spacing in tqdm(pipeline.train_dataloader, total=len(pipeline.train_dataloader), ascii=True, desc='Clustering', miniters=50):
-        tokens = tokenizer(batch)
-        spacing = torch.cat([spacing.unsqueeze(-1).unsqueeze(-1)]*64, dim=-1)
-        inputs = torch.cat((tokens, spacing), dim=1).detach().numpy()
-        if len(inputs)==1024:
-            kmeans = kmeans.partial_fit(inputs.reshape(inputs.shape[0], -1))
+        pipeline = train_val_test(device, path_prepared, dataset, encoder_selection, pretrained_encoder, single_output='intensity')
+        pipeline.load_model()
+        tokenizer = pipeline.model.combi_encoder
+        decoder = pipeline.model.AttentionDecoder
+        if not pipeline.model.training:
+            print('The model is correctly loaded under evaluation mode.')
         else:
-            print(f'Number of samples in the last batch: {len(inputs)}')
+            print('The model is not under evaluation mode, please check whether the model is correctly loaded.')
+            sys.exit(0)
 
-    # Define explainer, background samples are the cluster centers
-    bkgd_samples = kmeans.cluster_centers_
-    bkgd_samples = torch.from_numpy(bkgd_samples.reshape(bkgd_samples.shape[0],-1,64)).float()
-    explainer = shap.GradientExplainer(decoder, bkgd_samples, batch_size=1024)
+        # Define sample generator
+        sampler = get_sample(encoder_selection, path_result)
+        event_id_list = sampler.event_id_list.reset_index()
 
-    # Read events
-    warning = pd.read_hdf(path_result+'Analyses/Warning_SafeBaseline_current+acc_environment_profiles_not_pretrained.h5', key='results')
-    optimal_threshold = optimize_threshold(warning, 'SSSE', 'ROC')
-    warning = warning[(warning['threshold']==optimal_threshold)&warning['danger_recorded']]
+        # Cluster representative training samples
+        pipeline.create_dataloader(batch_size=1024)
+        kmeans = MiniBatchKMeans(n_clusters=1024, random_state=manual_seed, batch_size=1024)
+        for batch, spacing in tqdm(pipeline.train_dataloader, total=len(pipeline.train_dataloader), ascii=True, desc='Clustering', miniters=50):
+            tokens = tokenizer(batch)
+            spacing = torch.cat([spacing.unsqueeze(-1).unsqueeze(-1)]*64, dim=-1)
+            inputs = torch.cat((tokens, spacing), dim=1).detach().numpy()
+            if len(inputs)==1024:
+                kmeans = kmeans.partial_fit(inputs.reshape(inputs.shape[0], -1))
+            else:
+                print(f'Number of samples in the last batch: {len(inputs)}')
 
-    # Compute and save gradients for each event
-    results = []
-    ig_columns = [f'ig_{var}' for var in sampler.variables]
-    std_columns = [f'std_{var}' for var in sampler.variables]
-    for event_id, target_id in tqdm(warning[['event_id','target_id']].values, total=len(warning), ascii=True, desc='Attribution', miniters=50):
-        samples, proximity = sampler.get_item(event_id, target_id)
-        proximity = torch.from_numpy(proximity).float()
-        proximity = torch.cat([proximity.unsqueeze(-1).unsqueeze(-1)]*64, dim=-1)
-        sample_tokens = tokenizer(samples)
-        sample_inputs = torch.cat((sample_tokens, proximity), dim=1)
-        ig_matrix, var = explainer.shap_values(sample_inputs, return_variances=True, rseed=manual_seed)
-        ig_values = ig_matrix[:,:-1,:,0].sum(axis=2)
-        std = np.sqrt(var[0][:,:-1,:].sum(axis=2))
-        assert ig_values.shape == std.shape
-        if np.any(np.isnan(ig_values)):
-            Warning(f'There are NaN values in event {event_id} with target {target_id}.')
-        result = event_id_list[(event_id_list['event_id']==event_id)&(event_id_list['target_id']==target_id)]
-        result = result.sort_values(by='idx', ascending=False)[['event_id','target_id','time']]
-        result[ig_columns] = ig_values
-        result[std_columns] = std
-        results.append(result)
+        # Define explainer, background samples are the cluster centers
+        bkgd_samples = kmeans.cluster_centers_
+        bkgd_samples = torch.from_numpy(bkgd_samples.reshape(bkgd_samples.shape[0],-1,64)).float()
+        explainer = shap.GradientExplainer(decoder, bkgd_samples, batch_size=1024)
 
-    results = pd.concat(results, axis=0)
-    results.to_hdf(path_save+f'SafeBaseline_current+acc_environment_profiles_not_pretrained.h5', key='results', mode='w')
+        # Read events
+        warning = pd.read_hdf(path_result + f'Analyses/Warning_{model}.h5', key='results')
+        optimal_threshold = optimize_threshold(warning, 'SSSE', 'ROC')
+        warning = warning[(warning['threshold']==optimal_threshold)&warning['danger_recorded']]
+
+        # Compute and save gradients for each event
+        results = []
+        ig_columns = [f'ig_{var}' for var in sampler.variables]
+        std_columns = [f'std_{var}' for var in sampler.variables]
+        for event_id, target_id in tqdm(warning[['event_id','target_id']].values, total=len(warning), ascii=True, desc='Attribution', miniters=10):
+            samples, proximity = sampler.get_item(event_id, target_id)
+            proximity = torch.from_numpy(proximity).float()
+            proximity = torch.cat([proximity.unsqueeze(-1).unsqueeze(-1)]*64, dim=-1)
+            sample_tokens = tokenizer(samples)
+            sample_inputs = torch.cat((sample_tokens, proximity), dim=1)
+            ig_matrix, var = explainer.shap_values(sample_inputs, return_variances=True, rseed=manual_seed)
+            intensity = decoder(sample_inputs).squeeze().detach().numpy()
+            ig_values = ig_matrix[:,:-1,:,0].sum(axis=2)
+            std = np.sqrt(var[0][:,:-1,:].sum(axis=2))
+            assert ig_values.shape == std.shape
+            if np.any(np.isnan(ig_values)):
+                Warning(f'There are NaN values in event {event_id} with target {target_id}.')
+            result = event_id_list[(event_id_list['event_id']==event_id)&(event_id_list['target_id']==target_id)]
+            result = result.sort_values(by='idx', ascending=False)[['event_id','target_id','time']]
+            result[ig_columns] = ig_values
+            result[std_columns] = std
+            result['intensity'] = intensity
+            results.append(result)
+
+        results = pd.concat(results, axis=0)
+        results.to_hdf(path_save + f'{model}.h5', key='results', mode='w')
 
     print('--- Total time elapsed: ' + systime.strftime('%H:%M:%S', systime.gmtime(systime.time() - initial_time)) + ' ---')
     sys.exit(0)
