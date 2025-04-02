c4c72d49427fb7216eaaa2990b948658d3a3213c
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-04-02 17:08:26 +0200

    Refactor EKF reconstruction logic to improve clarity and maintainability

--- src_trajectory_reconstruction/reconstruction_utils/utils_ekf.py
+++ src_trajectory_reconstruction/reconstruction_utils/utils_ekf.py
@@ -33,8 +33,6 @@ def reconstruct_ego(df_ego, params, reverse=False):
     ## Measurement vector
     mv = veh['speed_comp'].values
     momega = veh['yaw_rate'].values
-    momega[(momega<1e-4)&(momega>=0)] = 1e-4 ## Driving straight
-    momega[(momega>-1e-4)&(momega<0)] = -1e-4
     macc = veh['acc_lon'].values
     ## Correct invalid measurements
     ### 1) the speed measurement is negative
@@ -53,53 +51,69 @@ def reconstruct_ego(df_ego, params, reverse=False):
 
     for filterstep in np.arange(1,m):
         ## Time Update (Prediction)
-        x[0] = x[0] + (1/x[4]**2) * (-x[3]*x[4]*np.sin(x[2]) -x[5]*np.cos(x[2]) +
-                                     x[5]*np.cos(x[2]+x[4]*dt[filterstep]) + 
-                                     (x[5]*x[4]*dt[filterstep]+x[3]*x[4])*np.sin(x[2]+x[4]*dt[filterstep]))
-        x[1] = x[1] + (1/x[4]**2) * (x[3]*x[4]*np.cos(x[2]) - x[5]*np.sin(x[2]) +
-                                     x[5]*np.sin(x[2]+x[4]*dt[filterstep]) +
-                                     (-x[5]*x[4]*dt[filterstep]-x[3]*x[4])*np.cos(x[2]+x[4]*dt[filterstep]))
-        x[2] = (x[2] + x[4] * dt[filterstep] + np.pi) % (2.0 * np.pi) - np.pi
-        x[3] = x[3] + x[5] * dt[filterstep]
+        delta_t = dt[filterstep]
+        if abs(x[4]) < 1e-3: ## Driving straight
+            delta_x = np.cos(x[2]) * (x[3]*delta_t + 0.5*x[5]*delta_t**2)
+            delta_y = np.sin(x[2]) * (x[3]*delta_t + 0.5*x[5]*delta_t**2)
+            ## Elements in the Jacobian of the Dynamic Matrix
+            a13 = -np.sin(x[2]) * (x[3]*delta_t + 0.5*x[5]*delta_t**2)
+            a14 = np.cos(x[2]) * delta_t
+            a15 = 0.
+            a16 = np.cos(x[2]) * 0.5*delta_t**2
+            a23 = np.cos(x[2]) * (x[3]*delta_t + 0.5*x[5]*delta_t**2)
+            a24 = np.sin(x[2]) * delta_t
+            a25 = 0.
+            a26 = np.sin(x[2]) * 0.5*delta_t**2
+        else:
+            delta_x = (1/x[4]**2) * (-x[3]*x[4]*np.sin(x[2]) -x[5]*np.cos(x[2]) +
+                                     x[5]*np.cos(x[2]+x[4]*delta_t) + 
+                                     (x[5]*x[4]*delta_t+x[3]*x[4])*np.sin(x[2]+x[4]*delta_t))
+            delta_y = (1/x[4]**2) * (x[3]*x[4]*np.cos(x[2]) - x[5]*np.sin(x[2]) +
+                                     x[5]*np.sin(x[2]+x[4]*delta_t) +
+                                     (-x[5]*x[4]*delta_t-x[3]*x[4])*np.cos(x[2]+x[4]*delta_t))
+            ## Elements in the Jacobian of the Dynamic Matrix
+            a13 = (-x[4]*x[3]*np.cos(x[2]) + x[5]*np.sin(x[2]) - x[5]*np.sin(delta_t*x[4]+x[2]) +
+                (delta_t*x[4]*x[5]+x[4]*x[3])*np.cos(delta_t*x[4]+x[2])) / x[4]**2
+            a14 = (-x[4]*np.sin(x[2]) + x[4]*np.sin(delta_t*x[4]+x[2])) / x[4]**2
+            a15 = (-delta_t*x[5]*np.sin(delta_t*x[4]+x[2]) + 
+                delta_t*(delta_t*x[4]*x[5]+x[4]*x[3])*np.cos(delta_t*x[4]+x[2]) - 
+                x[3]*np.sin(x[2]) + (delta_t*x[5] + x[3])*np.sin(delta_t*x[4]+x[2]))/x[4]**2 - (
+                    -x[4]*x[3]*np.sin(x[2]) - x[5]*np.cos(x[2]) +
+                    x[5]*np.cos(delta_t*x[4] + x[2]) + 
+                    (delta_t*x[4]*x[5] + x[4]*x[3])*np.sin(delta_t*x[4] + x[2])) *2 / x[4]**3
+            a16 = (delta_t*x[4]*np.sin(delta_t*x[4]+x[2]) - np.cos(x[2]) + np.cos(delta_t*x[4]+x[2])) / x[4]**2
+
+            a23 = (-x[4]*x[3]*np.sin(x[2]) - x[5]*np.cos(x[2]) + x[5]*np.cos(delta_t*x[4]+x[2]) -
+                (-delta_t*x[4]*x[5] - x[4]*x[3])*np.sin(delta_t*x[4]+x[2])) / x[4]**2
+            a24 = (x[4]*np.cos(x[2]) - x[4]*np.cos(delta_t*x[4] + x[2])) / x[4]**2
+            a25 = (delta_t*x[5]*np.cos(delta_t*x[4] + x[2]) -
+                delta_t*(-delta_t*x[4]*x[5]-x[4]*x[3])*np.sin(delta_t*x[4]+x[2]) + 
+                x[3]*np.cos(x[2]) + (-delta_t*x[5]-x[3])*np.cos(delta_t*x[4]+x[2]))/x[4]**2 - (
+                    x[4]*x[3]*np.cos(x[2]) - x[5]*np.sin(x[2]) + 
+                    x[5]*np.sin(delta_t*x[4]+x[2]) +
+                    (-delta_t*x[4]*x[5]-x[4]*x[3])*np.cos(delta_t*x[4]+x[2])) *2 / x[4]**3
+            a26 =  (-delta_t*x[4]*np.cos(delta_t*x[4]+x[2]) - np.sin(x[2]) + np.sin(delta_t*x[4] + x[2])) / x[4]**2
+
+        x[0] = x[0] + delta_x
+        x[1] = x[1] + delta_y
+        x[2] = (x[2] + x[4] * delta_t + np.pi) % (2.0 * np.pi) - np.pi
+        x[3] = x[3] + x[5] * delta_t
         x[4] = x[4]
         x[5] = x[5]
-
-        ## Calculate the Jacobian of the Dynamic Matrix JA
-        a13 = (-x[4]*x[3]*np.cos(x[2]) + x[5]*np.sin(x[2]) - x[5]*np.sin(dt[filterstep]*x[4]+x[2]) +
-               (dt[filterstep]*x[4]*x[5]+x[4]*x[3])*np.cos(dt[filterstep]*x[4]+x[2])) / x[4]**2
-        a14 = (-x[4]*np.sin(x[2]) + x[4]*np.sin(dt[filterstep]*x[4]+x[2])) / x[4]**2
-        a15 = (-dt[filterstep]*x[5]*np.sin(dt[filterstep]*x[4]+x[2]) + 
-               dt[filterstep]*(dt[filterstep]*x[4]*x[5]+x[4]*x[3])*np.cos(dt[filterstep]*x[4]+x[2]) - 
-               x[3]*np.sin(x[2]) + (dt[filterstep]*x[5] + x[3])*np.sin(dt[filterstep]*x[4]+x[2]))/x[4]**2 - (
-                   -x[4]*x[3]*np.sin(x[2]) - x[5]*np.cos(x[2]) +
-                   x[5]*np.cos(dt[filterstep]*x[4] + x[2]) + 
-                   (dt[filterstep]*x[4]*x[5] + x[4]*x[3])*np.sin(dt[filterstep]*x[4] + x[2])) *2 / x[4]**3
-        a16 = (dt[filterstep]*x[4]*np.sin(dt[filterstep]*x[4]+x[2]) - np.cos(x[2]) + np.cos(dt[filterstep]*x[4]+x[2])) / x[4]**2
-
-        a23 = (-x[4]*x[3]*np.sin(x[2]) - x[5]*np.cos(x[2]) + x[5]*np.cos(dt[filterstep]*x[4]+x[2]) -
-               (-dt[filterstep]*x[4]*x[5] - x[4]*x[3])*np.sin(dt[filterstep]*x[4]+x[2])) / x[4]**2
-        a24 = (x[4]*np.cos(x[2]) - x[4]*np.cos(dt[filterstep]*x[4] + x[2])) / x[4]**2
-        a25 = (dt[filterstep]*x[5]*np.cos(dt[filterstep]*x[4] + x[2]) -
-               dt[filterstep]*(-dt[filterstep]*x[4]*x[5]-x[4]*x[3])*np.sin(dt[filterstep]*x[4]+x[2]) + 
-               x[3]*np.cos(x[2]) + (-dt[filterstep]*x[5]-x[3])*np.cos(dt[filterstep]*x[4]+x[2]))/x[4]**2 - (
-                   x[4]*x[3]*np.cos(x[2]) - x[5]*np.sin(x[2]) + 
-                   x[5]*np.sin(dt[filterstep]*x[4]+x[2]) +
-                   (-dt[filterstep]*x[4]*x[5]-x[4]*x[3])*np.cos(dt[filterstep]*x[4]+x[2])) *2 / x[4]**3
-        a26 =  (-dt[filterstep]*x[4]*np.cos(dt[filterstep]*x[4]+x[2]) - np.sin(x[2]) + np.sin(dt[filterstep]*x[4] + x[2])) / x[4]**2
             
         JA = np.matrix([[1.0, 0.0, a13, a14, a15, a16],
                         [0.0, 1.0, a23, a24, a25, a26],
-                        [0.0, 0.0, 1.0, 0.0, dt[filterstep], 0.0],
-                        [0.0, 0.0, 0.0, 1.0, 0.0, dt[filterstep]],
+                        [0.0, 0.0, 1.0, 0.0, delta_t, 0.0],
+                        [0.0, 0.0, 0.0, 1.0, 0.0, delta_t],
                         [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
                         [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]], dtype=float)
 
         ## Calculate the Process Noise Covariance Matrix
-        s_pos = 0.5*max_acc*dt[filterstep]**2
-        s_psi = max_yaw_rate*dt[filterstep]
-        s_speed = max_acc*dt[filterstep]
-        s_omega = max_yaw_acc*dt[filterstep]
-        s_acc = max_jerk*dt[filterstep]
+        s_pos = 0.5*max_acc*delta_t**2
+        s_psi = max_yaw_rate*delta_t
+        s_speed = max_acc*delta_t
+        s_omega = max_yaw_acc*delta_t
+        s_acc = max_jerk*delta_t
 
         Q = np.diag([s_pos**2, s_pos**2, s_psi**2, s_speed**2, s_omega**2, s_acc**2])
 
@@ -168,25 +182,26 @@ def reconstruct_surrounding(veh, params):
 
     for filterstep in range(m):
         ## Time Update (Prediction)
-        x[0] = x[0] + dt[filterstep]*x[2]*np.cos(x[3])
-        x[1] = x[1] + dt[filterstep]*x[2]*np.sin(x[3])
+        delta_t = dt[filterstep]
+        x[0] = x[0] + delta_t*x[2]*np.cos(x[3])
+        x[1] = x[1] + delta_t*x[2]*np.sin(x[3])
         x[2] = x[2]
         x[3] = (x[3]+ np.pi) % (2.0*np.pi) - np.pi
 
         ## Calculate the Jacobian of the Dynamic Matrix JA
-        a13 = dt[filterstep]*np.cos(x[3])
-        a14 = -dt[filterstep]*x[2]*np.sin(x[3])
-        a23 = dt[filterstep]*np.sin(x[3])
-        a24 = dt[filterstep]*x[2]*np.cos(x[3])
+        a13 = delta_t*np.cos(x[3])
+        a14 = -delta_t*x[2]*np.sin(x[3])
+        a23 = delta_t*np.sin(x[3])
+        a24 = delta_t*x[2]*np.cos(x[3])
         JA = np.matrix([[1.0, 0.0, a13, a14],
                         [0.0, 1.0, a23, a24],
                         [0.0, 0.0, 1.0, 0.0],
                         [0.0, 0.0, 0.0, 1.0]], dtype=float)
 
         ## Calculate the Process Noise Covariance Matrix
-        s_pos = 0.5*max_acc*dt[filterstep]**2
-        s_psi = max_yaw_rate*dt[filterstep]
-        s_speed = max_acc*dt[filterstep]
+        s_pos = 0.5*max_acc*delta_t**2
+        s_psi = max_yaw_rate*delta_t
+        s_speed = max_acc*delta_t
 
         Q = np.diag([s_pos**2, s_pos**2, s_speed**2, s_psi**2])
 
