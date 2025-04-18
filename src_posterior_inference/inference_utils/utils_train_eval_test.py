a32c29385939368e439f6cae81f9564b4c15f89e
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-04-19 00:23:28 +0200

    Remove debug print statement from model loading in train_val_test class

--- src_posterior_inference/inference_utils/utils_train_eval_test.py
+++ src_posterior_inference/inference_utils/utils_train_eval_test.py
@@ -304,7 +304,6 @@ class train_val_test():
                 self.path_output = self.path_prepared + f'PosteriorInference/{self.dataset_name}/pretrained_all/{self.encoder_name}/'
         if mixrate<=1 and 'mixed' not in self.path_output:
             self.path_output = f'{self.path_output}mixed{mixrate}/'
-        print(os.listdir(self.path_output))
         final_model = [f for f in os.listdir(self.path_output) if f.endswith('.pth')][0]
         final_model = os.path.join(self.path_output, final_model)
         self.model.load_state_dict(torch.load(final_model, map_location=torch.device(self.device), weights_only=True))        
