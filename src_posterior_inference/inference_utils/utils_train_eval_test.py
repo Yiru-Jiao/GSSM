eab32d1621cc38210fa5220244a16e721adb6f9e
Yiru Jiao <y.jiao-1@tudelft.nl>
2025-04-30 23:36:24 +0200

    Update mixrate values for ArgoverseHV and highD in DataOrganiser and adjust model filtering in vote_conflicting_target for consistency.

--- src_posterior_inference/inference_utils/utils_train_eval_test.py
+++ src_posterior_inference/inference_utils/utils_train_eval_test.py
@@ -26,7 +26,7 @@ def set_experiments(stage=[1,2,3]):
         exp_config.extend([
             [['SafeBaseline','ArgoverseHV'], ['current']],
             [['SafeBaseline','highD'], ['current']],
-            # [['SafeBaseline','ArgoverseHV','highD'], ['current']],
+            [['SafeBaseline','ArgoverseHV','highD'], ['current']],
         ])
     if 3 in stage: # add extra features
         exp_config.extend([
@@ -127,7 +127,7 @@ class train_val_test():
                 inducing_out = model2use(inducing_points)
         return inducing_out
     
-    def mask_xts(self, x, model2use, drop_rate=0.4):
+    def mask_xts(self, x, model2use, drop_rate=0.5):
         if isinstance(x, list) and len(x)==3 and model2use.training:
             # randomly mask the time series input to avoid position bias
             self.random_mask = (torch.rand_like(x[2], requires_grad=False) > drop_rate).float()
